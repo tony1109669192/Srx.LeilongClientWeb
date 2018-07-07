048b98303dfa209b930419cd92382f5a5d57733b
1109669192@qq.com <1109669192@qq.com>
2018-07-07 12:59:48 +0800

    初始化Compass

---
+++ config.rb
@@ -0,0 +1,23 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "E:/workspaces/Srx.LeilongClientWeb"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+generated_images_path = "E:/workspaces/Srx.LeilongClientWeb/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
